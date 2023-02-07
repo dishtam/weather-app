0cd17de71cb68247af6a493ea591cc54c0eff428
Rohit Roy <rohhiiiit@gmail.com>
2023-02-07 10:39:32 +0530

    changes made in the app.js file and a function is made  added

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const https = require('https');
 const app = express();
 
 app.get("/",function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Silchar&units=metric&appid=47b88cf98b06d744d58f1965454534f6";
+    var city = "Silchar";
+    const API = "47b88cf98b06d744d58f1965454534f6";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&callback=test&appid="+API;
     https.get(url,function(response){
-      console.log(response);
+      let data = '';
+	response.on('data',function(chunk){
+		data += chunk;
+	});
+	response.on('end',function(){
+		console.log(data);
+	});
     });
     res.send("Server is running");
 });
